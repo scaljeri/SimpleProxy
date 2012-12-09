3821c6c465420f8add9c3317297c1909edb7b53c
Lucas Calje <lucas@calje.eu>
2012-12-09 22:06:51 +0100

    refactored html and added an event listener to route-delete

--- admin/resources/js/admin.js
+++ admin/resources/js/admin.js
@@ -1,10 +1,30 @@
 (function($) {
 	if ( !window.admin )
 		window.admin = {} ;
+
+	var name   = "defaults.json" ; 	// name of the selected configuration file
+	var config = null ; 		// contains the loaded configuration data
 	
 	window.admin.loadConfigs = function() {
 			doAjax( {url: '/admin/config.json', success: insertConfigs} ) ;
-			doAjax( {url: '/admin/config/defaults.json', success: fillForm} ) ;
+			doAjax( {url: '/admin/config/' + name, success: fillForm} ) ;
+
+			// bind to route actions
+			$('#routes').off('click', 'span') // event delegation
+				    .on('click', 'span', function(e) {
+					var $this = $(this) ;
+					var index = $this.parent().parent().attr('data-index') ;
+					var atype = $(this).attr("data-type") ;
+
+					if ( atype == 'delete' ) {
+						$('#routes li[data-index="' + index + '"]').fadeOut(function(){ 
+							$(this).remove();
+							if ( $('#routes li').length == 0 ) 
+								$('#routes').append(window.admin.templates.noRoutesDefined({})) ;	
+						}) ;
+						//doAjax( { url: '/admin/config/route/' + name, data: {
+					}
+			}) ;
 		};
 	window.admin.manipulate = function(e) {
 			var action = $(e.target).attr('data-manip') ;
@@ -32,6 +52,7 @@
 		};
 
 	function fillForm(json) {
+		config = json ;
 		if ( json.description )	
 			$('#description').html(json.description) ;
 
@@ -47,24 +68,19 @@
 		if ( json['ignore-params'] )
 			$('#ignore-params').val(json['ignore-params']) ;
 
-		if ( json.routes && json.routes.length > 0 ) {
-			$('#routes').html(window.admin.templates.createRoutes(json)) ;
-/*
-			$('#routes').empty() ;
-			for( var i = 0; i < json.routes.length; i++ ) {
-				var route = json.routes[i] ;
-				var routeTo = route.host ? route.host + ':' + route.port : route.path ; 
-				if ( route.host )
-					routeTo += (/^\//.test(route.path) ? '' : '/') + route.path ;
-				$('<a href="#"><div class="route-' + i + '" data-json="' + JSON.stringify(route) + '"><span>' + route.url + '</span> - <span>' + routeTo + '</span></div></a>').appendTo('#routes') ;
-			}
-*/
+		if ( json.routes && json.routes.length > 0 )  {
+			$('#routes').empty() 
+	    			.append(window.admin.templates.createRoutes(json)) ;
 		}
 
 	}
 
+	function insertRouteConfig(json) {
+
+	}
+
 	function insertConfigs(json) {
-		var s = $('#config-select') ;
+		var s = $('#config-select').empty() ;
 		for(var key in json) {
     			$("<option />", {value: key, text: json[key].replace('.json','')}).appendTo(s) ;
 		}
--- admin/resources/js/handlebars-templates.js
+++ admin/resources/js/handlebars-templates.js
@@ -2,39 +2,178 @@ if ( ! window.admin )
 	window.admin = {} ;
 
 window.admin.tooltips = {
-	"edit-route": "this is a tooltip",	
-	"delete-route": "this is a tooltip - delete"	
+	"create-config": "Create a new configuration",
+	"delete-config": "Delete selected configuration",
+	"info-hostname": "remote host name (--host)",
+	"info-remote-port": "remote port number (--port)",
+	"info-proxy-only": "No request will be served from cache, but each (new) response is written to cache (--act-as-proxy)",
+	"info-ignore-params": "A comma seperated list with parameters (POST or GET) to be ignored for cache requests lookup (--ignore-params",
+	"route-create": "Create new route",
+	"route-edit": "Edit route configuration below",	
+	"route-delete": "Delete this route configuration",
+	"route-incoming-url": "proxy (and cache) all request. If a request is already cached, it will be updated with the new response (--map-url)",
+	"route-cache": "Specify if caching should be enabled/disabled (--map-cache)",
+	"route-to": "Route a specific url to an other hostname/ip or to the local filesystem",
+	"route-to-host": "proxy the request to an other host. If only an absolute path is defined the default hostname/port is used (--map-to-url)",
+	"route-to-filesystem": "forward the request to the local filesystem (--map-to-fs)",
+	"route-ignore-extensions": "A comma seperareted list with extensions which should not be mapped (--map-ignore-extensions)"
 }
 
 
 
 window.admin.templates = {
+	noRoutesDefined: Handlebars.compile( '<div id="no-routes">No routes configured</div>' ),
+	addRouteConfig: Handlebars.compile(
+		[
+			'<section>',
+				'<header style="position:relative">',
+					'<h4>Route URL to host/ip or local filesystem:</h4>',
+					'<div style="position:absolute;top:0;right:0">',
+						'{{#tooltipLink text="route-create" icon="add"}}{{/tooltipLink}}',
+					'</div>',
+				'</header',
+                                '<article>',
+					'<div class="route-input">',
+                                        	'<label>Incoming URL</label>',
+                                        	'<input type="text" placeholder="/admin/configs/json/localhost.json">',
+					'</div>',
+					'{{#tooltipLink text="route-incoming-url" icon="information"}}{{/tooltipLink}}',
+                                '</article>',
+                                '<article>',
+					'<div class="route-input">',
+                                        	'<label for="proxy">Cache request</label>',
+                                        	'<div class="value-container" style="display:inline-block;width:206px">',
+                                                	'<input type="radio" name="route-cache" value="true">true',
+                                                	'<input type="radio" name="route-cache" value="false">false',
+                                        	'</div>',
+                                        '</div>',
+					'{{#tooltipLink text="route-cache" icon="information"}}{{/tooltipLink}}',
+                                '</article>',
+                                '<article>',
+					'<div class="route-input">',
+                                        	'<label>Route to:</label>',
+                                        	'<input type="radio" name="route-to" value="hostname">hostname/ip',
+                                        	'<input type="radio" name="route-to" value="filesystem">Filesystem',
+					'</div>',
+					'{{#tooltipLink text="route-to" icon="information"}}{{/tooltipLink}}',
+                                '</article>',
+                               	'<article>',
+					'<div id="route-to-hostname" class="hidden">',
+						'<div class="route-input">',
+                                        		'<label>Hostname</label>',
+                                        		'<input name="route-to-hostname" type="text" placeholder="localhost" style="width:160px">',
+                                        		'<input type="route-to-port" text" placeholder="80" style="width:30px;margin-left:10px;">',
+						'</div>',
+						'{{#tooltipLink text="route-to-host" icon="information"}}{{/tooltipLink}}',
+					'</div>',
+					'<div id="route-to-fs" class="hidden">',
+						'<div class="route-input">',
+                                        		'<label>Hostname</label>',
+                                        		'<input name="route-to-fs" type="text" placeholder="/Users/www/">',
+						'</div>',
+						'{{#tooltipLink text="route-to-fs" icon="information"}}{{/tooltipLink}}',
+					'</div>',
+                                '</article>',
+                                '<article>',
+                                        	'<label for="proxy">Ignore specific extensions</label>',
+                                        	'<div style="display:inline-block;vertical-align:top">',
+                                                	'<input type="text" placeholder="jsp,asp,php" style="margin-right: 7px">',
+							'{{#tooltipLink text="route-ignore-extensions" icon="information"}}{{/tooltipLink}}',
+						'</div>',
+                                '</article>',
+			'</section>'
+		].join('')
+	),
+	createConfigLoader: Handlebars.compile(
+		[
+			'<section>',
+				'<div id="config-loading">',
+					'Loading configuration files',
+                                	'<div class="spinner"></div>',
+                        	'</div>',
+	
+                        	'<div id="configs" class="hidden">',
+                        		'<label for="config-select" class="config-msg">Choose config file: </label>',
+                                	'<select id="config-select"></select>',
+	
+					'{{#tooltipLink id="delete-config" class="hidden" text="delete-config" icon="delete"}}{{/tooltipLink}}',
+					'{{#tooltipLink text="create-config" icon="add"}}{{/tooltipLink}}',
+                        	'</div>',
+			'</section>'
+		].join('')
+	),
+
+	createGlobalSettings: Handlebars.compile(
+		[
+			'<section',
+				'<acticle>', <!-- HOSTNAME -->
+                        		'<label for="remote-host">Remote host</label>',
+                        		'<input id="remote-host" type="text" placeholder="localhost" class="tooltip">',
+					'{{#tooltipLink text="info-hostname" icon="information"}}{{/tooltipLink}}',
+                  		'</article>',
+
+                  		'<article>', <!-- REMOTE PORT -->
+                        		'<label for="remote-port">Remote port</label>',
+                        		'<input id="remote-port" type="text" placeholder="80">',
+					'{{#tooltipLink text="info-remote-port" icon="information"}}{{/tooltipLink}}',
+				'</article>',
+                  		'<article>', <!-- ACT AS PROXY ONLY -->
+                                        '<label for="proxy">Act as proxy only</label>',
+                                        '<div class="value-container" style="display:inline-block;width:206px">',
+                                                '<input id="proxy-only-true" type="radio" name="act-as-proxy" value="true">true',
+                                                '<input id="proxy-only-false" type="radio" name="act-as-proxy" value="false">false',
+                                        '</div>',
+					'{{#tooltipLink text="info-remote-proxy" icon="information"}}{{/tooltipLink}}',
+                  		'</article>',
+                  		'<article>', <!-- IGNORE PARAMETERS -->
+                        		'<label for="proxy">ignore parameter</label>',
+                        		'<input id="ignore-params" type="text" placeholder="_dc,r">',
+					'{{#tooltipLink text="info-ignore-params" icon="information"}}{{/tooltipLink}}',
+                  		'</article>'
+		].join('')
+	),
+
 	createTooltipLink: Handlebars.compile(
                 [
                         '<span ',
-                                'class="{{classes}} tooltip ss_sprite ss_{{icon}}" ',
-                                'data-tip="{{text}}">',
+                                'class="{{classes}} tooltip hand ss_sprite ss_{{icon}}" ',
+                                'data-tip="{{text}}" data-type="{{data-type}}">',
                                         '&nbsp;',
                         '</span>'
-                ].join('')),
+                ].join('')
+	),
+
 	createRoutes: Handlebars.compile(
 		[
-			'{{#each routes}}',
-				'<div>',
-					'<li>',
-						'{{url}} -- {{#createPath}}{{host}} {{port}} {{path}}{{/createPath}}',
-						'<div style="float:right">',
-							'{{#tooltipLink text="edit-route" icon="page_white_edit"}}{{/tooltipLink}}',
-							'{{#tooltipLink text="delete-route" icon="delete"}}{{/tooltipLink}}',
-						'</div>',
-					'</li>',
-				'<div>',
-			'{{/each}}'
+			'{{#eachWithIndex routes}}',
+				'<li data-index="{{index}}">',
+					'{{url}} -- {{#createPath}}{{host}} {{port}} {{path}}{{/createPath}}',
+					'<input type="hidden" name="route-url"   value="{{url}}"/>',
+					'<input type="hidden" name="route-cache" value="{{cache}}"/>',
+					'<input type="hidden" name="route-host"  value="{{host}}"/>',
+					'<input type="hidden" name="route-port"  value="{{port}}"/>',
+					'<input type="hidden" name="route-path"  value="{{path}}"/>',
+					'<input type="hidden" name="route-ignore-extensions" value="{{ignore-extensions}}"/>',
+					'<div style="float:right">',
+						'{{#tooltipLink text="route-edit" icon="page_white_edit" data-type="edit"}}{{/tooltipLink}}',
+						'{{#tooltipLink text="route-delete" icon="delete" data-type="delete"}}{{/tooltipLink}}',
+					'</div>',
+				'</li>',
+			'{{/eachWithIndex}}'
 	       	].join('')
 	)
 };
 
 Handlebars.registerPartial('createTooltipLink', window.admin.templates.createTooltipLink ) ;
+
+Handlebars.registerHelper("eachWithIndex", function(array, block) {
+	var buffer = '' ;
+	for (var i = 0; i < array.length; i++) {
+		array[i].index = i ;
+		buffer += block.fn(array[i]);
+	}
+	return buffer;
+});
 
 Handlebars.registerHelper('createPath', function(options) {
 	
@@ -57,7 +196,8 @@ Handlebars.registerHelper('tooltipLink', function(options) {
 		this,
 		{
 			icon: options.hash.icon,
-			text: window.admin.tooltips[options.hash.text]
+			text: window.admin.tooltips[options.hash.text],
+			"data-type": options.hash["data-type"]
 		}
 	))}
 ) ;
