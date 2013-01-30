6a48ffe8078e0e59b77215c67f7184342b3b44f7
Lucas Calje <lucas@calje.eu>
2013-01-30 21:34:00 +0100

    implemented emberjs templates

--- admin/resources/js/app/router.js
+++ admin/resources/js/app/router.js
@@ -3,12 +3,33 @@ Sp.Router.map(function(match) {
 });
 
 Sp.IndexRoute = Ember.Route.extend({
+    renderTemplate: function(controller, model) {
+        this.render('index');
+
+        this.render('rotatable', {into: 'index', outlet: 'config'}) ;
+        this.render('clickable', {into: 'rotatable', outlet: 'front'}) ;
+        this.render('menuconfig', {into: 'clickable'}) ;
+        this.render('clickable', {into: 'rotatable', outlet: 'back'}) ;
+        this.render('configuration', {into: 'clickable'}) ;
+
+        this.render('rotatable', {into: 'index', outlet: 'logging'}) ;
+        this.render('clickable', {into: 'rotatable', outlet: 'front'}) ;
+        this.render('menulogging', {into: 'clickable'}) ;
+        this.render('configuration', {into: 'rotatable', outlet: 'back'}) ;
+
+        this.render('rotatable', {into: 'index', outlet: 'realtime'}) ;
+        this.render('clickable', {into: 'rotatable', outlet: 'front'}) ;
+        this.render('menurealtime', {into: 'clickable'}) ;
+        this.render('realtime', {into: 'rotatable', outlet: 'back'}) ;
+
+        this.render('rotatable', {into: 'index', outlet: 'search'}) ;
+        this.render('clickable', {into: 'rotatable', outlet: 'front'}) ;
+        this.render('menusearch', {into: 'clickable'}) ;
+        this.render('search', {into: 'rotatable', outlet: 'back'}) ;
+    }
   /*
   model: function(){
 	return Sp.ConfigModel.create() ;
-  },
-  renderTemplate: function(controller, model) {
-    this.render('index');
   }
   */
 });
